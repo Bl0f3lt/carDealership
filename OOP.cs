b64340332d97ba429703a1c12775ac59308bfd76
Bl0f3lt <Scott.Miller06@outlook.com>
2024-06-16 14:43:34 +0100

    Added code to add new car and display entries. Not tested yet. Also developed dictionary system more. Learnt about this.

--- OOP.cs
+++ OOP.cs
@@ -23,14 +23,34 @@ public class DealershipProgram
 
 		UserInterface.DisplayOptions();
 		int selection = UI1.getUserSelection();
-		UserInterface.callOptionMethod(selection);
+		UI1.callOptionMethod(selection);
 	}
 }
 
 
 class UserInterface
 {
-	
+    bool carMapExists 
+    { get; set; }
+    Dictionary<string, object>? carMap
+    { get; set; }
+
+    public UserInterface(Dictionary<string,object>? carMap = null)
+	{
+		//Constructor. Can be used to import existing dictionaries. Creates one if not.
+		if (carMap != null)
+		{
+			this.carMapExists = true;
+			this.carMap = carMap;
+		}
+		else
+		{
+            this.carMapExists = false;
+			this.carMap = new Dictionary<string, object>();
+		}
+
+	}
+
 	public static void DisplayOptions()
 	{
 		//Displays a Console menu and gets users selection
@@ -59,7 +79,7 @@ class UserInterface
                 Convert.ToInt32(userEntry);
 				validInt = true;
             }
-            catch (Exception e)
+            catch
             {
                 Console.WriteLine("Please Enter a Valid Number! Try Again.");
             }
@@ -78,8 +98,58 @@ class UserInterface
 		return userSelection;
     }
 
+	public string getNonNullString()
+	{
+		bool isNull = true;
+		string? capturedString = "";
+        while (isNull) {
+            capturedString = Console.ReadLine();
+			if (capturedString != null) { isNull = false; }
+			else { Console.WriteLine("Null value entered! Please enter non null value"); }
+        }
+		return capturedString;
+	}
 
-	public static void callOptionMethod(int userSelection)
+	public object getCarDetails()
+	{
+		Console.WriteLine("Enter model Name; ");
+		string modelName = getNonNullString();
+        Console.WriteLine("Enter manufacturer Name; ");
+        string manufacturerName = getNonNullString();
+        Console.WriteLine("Enter colour Name; ");
+        string carColour = getNonNullString();
+        Console.WriteLine("Enter car age; ");
+        int carAge = getInt();
+
+		return new Car(modelName,manufacturerName,carColour,carAge);
+
+    }
+
+
+	private void createNewEntry()
+	{
+        //Dictionary<string,object> carMap = new Dictionary<string,object>(); 
+        Console.WriteLine("Enter Number plate: ");
+		//string numPlate = Console.ReadLine();
+		string numPlate = getNonNullString();
+		object car1 = getCarDetails();
+
+		this.carMap[numPlate] = car1;
+
+    }
+
+	private void displayEntries()
+	{
+		int entryNum = 0;
+		foreach (KeyValuePair<string, object> elements in this.carMap)
+		{
+			Console.WriteLine("{0} : {1}", entryNum,elements.Key);
+			entryNum++;
+		}
+	}
+
+
+	public void callOptionMethod(int userSelection)
 	{
 		//Switch Case to Translate User input to program function.
 		//Console outputs are for debug and not final.
@@ -87,10 +157,18 @@ class UserInterface
 		{
 			case 1:
 				Console.WriteLine("Create New Entry");
+				createNewEntry();
 				break;
 
 			case 2:
-				Console.WriteLine("Display Entries");
+				Console.WriteLine("Displaying Entries");
+				if (this.carMapExists != true) {
+					displayEntries();
+				}
+				else
+				{
+					Console.WriteLine("Cannot output entries as no entries exist!.");
+				}
 				break;
 
 			case 3:
